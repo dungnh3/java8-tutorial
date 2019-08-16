6eb206753f6df6d68355ed2eaa62157f0af3b424
nghoangdung <nguyenhoangdung300993@gmail.com>
2019-08-16 14:55:29 +0700

    Initial project

---
+++ src/main/java/com/github/dungnh3/java8tutorial/Java8TutorialApplication.java
@@ -0,0 +1,13 @@
+package com.github.dungnh3.java8tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8TutorialApplication.class, args);
+	}
+
+}
